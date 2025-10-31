fd7b48cf642b0f1f52b18c8ac9dd08bf4b743a8c
ASUS <faris13abiyu@gmail.com>
2025-10-31 15:25:47 +0700

    Pembuatan button

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -3,9 +3,12 @@ package com.example.pertemuan5
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -20,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -80,5 +84,17 @@ fun FormDataDiri(modifier: Modifier
             thickness = dimensionResource(R.dimen.divider_tipis),
             color = Color.DarkGray
         )
+
+        Button (
+            modifier = Modifier.fillMaxWidth(1f),
+            enabled = textalamat.isEmpty(),
+            onClick = {
+                nama=textNama
+                jenis=textJK
+                alamat=textalamat
+            }
+        ){
+            Text(stringResource(R.string.submit))
+        }
     }
 }
